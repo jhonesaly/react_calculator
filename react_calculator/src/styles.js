5e05919820ed8e942ee43a952f4552417b4ca134
Jhones <jhonesaly@gmail.com>
2023-11-03 11:17:43 -0300

    feat centralizando conteúdo

--- react_calculator/src/styles.js
+++ react_calculator/src/styles.js
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: #FAFABA;
+    background-color: #FAFAFA;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
