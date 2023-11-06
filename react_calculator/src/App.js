501c9d79283a1f516c22a8f3e22ad315b7dd100f
Jhones <jhonesaly@gmail.com>
2023-11-06 17:18:33 -0300

    feat: função de adicionar números

--- react_calculator/src/App.js
+++ react_calculator/src/App.js
@@ -2,37 +2,44 @@
 import Input from './components/Input';
 import Button from './components/Button';
 
-import { Container, Content, Row, Column } from "./styles";
+import { Container, Content, Row } from "./styles";
+import { useState } from 'react';
 
 const App = () => {
+  
+  const [currentNumber, setCurrentNumber] = useState(0);
+  
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  }
   return (
       <Container>
         <Content>
-          <Input />
+          <Input value={currentNumber}/>
           <Row>
-            <Button label="7"/>
-            <Button label="8"/>
-            <Button label="9"/>
+            <Button label="7" onClick={() => handleAddNumber('7')}/>
+            <Button label="8" onClick={() => handleAddNumber('8')}/>
+            <Button label="9" onClick={() => handleAddNumber('9')}/>
             <Button label="B"/>
             <Button label="C"/>
 
           </Row>
           <Row>
-            <Button label="4"/>
-            <Button label="5"/>
-            <Button label="6"/>
-            <Button label="X"/>
+            <Button label="4" onClick={() => handleAddNumber('4')}/>
+            <Button label="5" onClick={() => handleAddNumber('5')}/>
+            <Button label="6" onClick={() => handleAddNumber('6')}/>
+            <Button label="x"/>
             <Button label="/"/>
           </Row>
           <Row>
-            <Button label="1"/>
-            <Button label="2"/>
-            <Button label="3"/>
+            <Button label="1" onClick={() => handleAddNumber('1')}/>
+            <Button label="2" onClick={() => handleAddNumber('2')}/>
+            <Button label="3" onClick={() => handleAddNumber('3')}/>
             <Button label="+"/>
             <Button label="-"/>
           </Row>
           <Row>
-            <Button label="0"/>
+            <Button label="0" onClick={() => handleAddNumber('0')}/>
             <Button label=","/>
             <Button label="^"/>
             <Button label="A"/>
